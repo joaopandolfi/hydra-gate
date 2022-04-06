94eb5b1c828542318d1a862318940912a0a466c3
joaopandolfi <joaopandolfi@gmail.com>
2022-04-06 14:41:04 -0300

    test

--- golang/web/controllers/lambda/routes.go
+++ golang/web/controllers/lambda/routes.go
@@ -5,5 +5,6 @@ import "hydra_gate/web/server"
 // SetupRouter -
 func (c *controller) SetupRouter(s *server.Server) {
 	c.s = s
-	s.R.PathPrefix("/").Handler(c)
+	c.s.R.HandleFunc("/x", c.ServeHTTP).Methods("POST", "GET", "HEAD")
+
 }
